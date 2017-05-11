02b81570aa5728f3f191d7a789e1a045f04d72da
Ian Macalinao <me@ian.pw>
2017-05-11 00:07:46 -0500

    favicons

--- site.hs
+++ site.hs
@@ -32,6 +32,10 @@ main = hakyll $ do
         route   idRoute
         compile copyFileCompiler
 
+    match "favicon.ico" $ do
+        route   idRoute
+        compile copyFileCompiler
+
     match (fromList ["about.rst", "contact.markdown"]) $ do
         route   $ setExtension "html"
         compile $ pandocCompiler
