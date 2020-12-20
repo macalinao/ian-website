e14ba6e316de5fac402e149858b406b9d129683d
Ian Macalinao <github@igm.pub>
2020-12-19 20:45:20 -0600

    use new css2 google font endpoint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,7 @@ import { globalStyles } from "~src/lib/styles/globalStyles";
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <AppWrapper>
-      <GoogleFonts href="https://fonts.googleapis.com/css?family=Merriweather:300,700&display=swap" />
+      <GoogleFonts href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300;700&display=swap" />
       <Global styles={globalStyles} />
       <Head>
         <link
