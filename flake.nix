5a6670230e15f545e42f3128439c26db1c0bb1c5
Ian Macalinao <github@igm.pub>
2021-09-02 14:53:47 -0400

    Migrate to nix flakes (#14)
    * Migrate to nix flakes
    
    * flake, github actions
    
    * gte

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Ian's personal website.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { flake-utils, nixpkgs, ... }:
+    (flake-utils.lib.eachSystem [
+      "aarch64-darwin"
+      "x86_64-darwin"
+      "x86_64-linux"
+    ] (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            (_: pkgs: {
+              update-static-assets =
+                pkgs.writeShellScriptBin "update-static-assets" ''
+                  set +x
+                  GIT_ROOT=$(git rev-parse --show-toplevel)
+                  if [ ! -e ./static-assets ]; then
+                    echo "Incorrect directory; must have static-assets folder."
+                    exit 1
+                  fi
+                  ${pkgs.awscli2}/bin/aws s3 sync --exclude '.*' \
+                   --profile ian --acl public-read \
+                    $GIT_ROOT/static-assets s3://static.ian.pw/images/
+                '';
+            })
+          ];
+        };
+      in { devShell = import ./shell.nix { inherit pkgs; }; }));
+}
--- shell.nix
+++ shell.nix
@@ -1,18 +1,11 @@
 { pkgs ? import ./nix }:
 
-let
-  pre-commit-check = pkgs.nix-pre-commit-hooks.run {
-    src = ./.;
-    hooks = {
-      nixfmt.enable = true;
-      shellcheck.enable = true;
-      prettier.enable = true;
-    };
-  };
-in with pkgs;
+with pkgs;
 mkShell {
-  buildInputs = [ yarn nodejs awscli2 update-static-assets ];
-  shellHook = ''
-    ${pre-commit-check.shellHook}
-  '';
+  buildInputs = [
+    (yarn.override { nodejs = nodejs-14_x; })
+    nodejs-14_x
+    awscli2
+    update-static-assets
+  ];
 }
