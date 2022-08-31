30da36d7de55817ca0b9b9a65268f7118be9e225
Ian Macalinao <github@igm.pub>
2022-08-31 14:57:16 -0500

    Fix 404 pages (#76)

--- src/lib/content/posts.ts
+++ src/lib/content/posts.ts
@@ -1,6 +1,7 @@
 import { default as matter } from "@gr2m/gray-matter";
 import * as fs from "fs/promises";
 import path from "path";
+import { default as invariant } from "tiny-invariant";
 
 const postsDir = path.join(process.cwd(), "content/posts");
 
@@ -26,29 +27,38 @@ export interface IPost {
 export const getAllPosts = async (): Promise<readonly IPost[]> => {
   const posts = await fs.readdir(postsDir);
   return await Promise.all(
-    posts.map(async (post) => {
+    posts.map(async (post): Promise<IPost> => {
       const postID = post.split(".").slice(0, -1).join(".");
-      return await getPostByID(postID);
+      const postResult = await getPostByID(postID);
+      invariant(postResult);
+      return postResult;
     })
   );
 };
 
-export const getPostByID = async (postID: string): Promise<IPost> => {
-  const source = await fs.readFile(`${postsDir}/${postID}.md`);
-  const { content, data: dataUnknown } = matter(source.toString());
-  const data = dataUnknown as IPost;
-  const publishedAt = new Date(postID.split("-").slice(0, 3).join("-"));
-  return {
-    id: postID,
-    title: data.title,
-    draft: data.draft === true,
-    publishedAt: publishedAt.toISOString(),
-    content,
-    path: `/posts/${postID}`,
-    incomplete: data.incomplete === true,
-    description: data.description ?? null,
-    hasMath: data.hasMath === true,
-    banner: data.banner ?? null,
-    tags: data.tags ?? [],
-  };
+export const getPostByID = async (postID: string): Promise<IPost | null> => {
+  try {
+    const source = await fs.readFile(`${postsDir}/${postID}.md`);
+    const { content, data: dataUnknown } = matter(source.toString());
+    const data = dataUnknown as IPost;
+    const publishedAt = new Date(postID.split("-").slice(0, 3).join("-"));
+    return {
+      id: postID,
+      title: data.title,
+      draft: data.draft === true,
+      publishedAt: publishedAt.toISOString(),
+      content,
+      path: `/posts/${postID}`,
+      incomplete: data.incomplete === true,
+      description: data.description ?? null,
+      hasMath: data.hasMath === true,
+      banner: data.banner ?? null,
+      tags: data.tags ?? [],
+    };
+  } catch (e) {
+    if ((e as { code?: string }).code === "ENOENT") {
+      return null;
+    }
+    throw e;
+  }
 };
